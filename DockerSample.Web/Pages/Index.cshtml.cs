239a98558ff53d7f9595583532447e9f10d9193a
Galip Tolga Erdem <galiperdem@gmail.com>
2021-01-07 20:02:09 +0300

    initial

---
+++ DockerSample.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DockerSample.Web.Pages
+{
+    public class Index : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public void OnPostLoginAsync()
+        {
+        }
+    }
+}
